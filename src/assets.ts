920caeeab2f6b568809d7b14ffb46921e2996c69
Roman Rios <romanrios@live.com>
2023-12-22 17:20:38 -0300

    asset

--- src/assets.ts
+++ src/assets.ts
@@ -8,7 +8,7 @@ export const manifest: ResolverManifest = {
             {
                 myModel: "b.glb",
                 fullscreen: "fullscreen.png",
-                scene: "scene.gltf"
+                // scene: "scene.gltf"
 
             }
         }
